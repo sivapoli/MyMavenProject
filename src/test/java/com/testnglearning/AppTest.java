9a43818d9122becbb804e2b7c25fa8f039f944ad
sivapoli <siva.poli22@gmail.com>
2016-01-07 21:11:10 -0600

    second commit

---
+++ src/test/java/com/testnglearning/AppTest.java
@@ -0,0 +1,21 @@
+package com.testnglearning;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+
+//run Java file through command prompt-maven
+
+//run testng.xml from maven commandline
+
+//surefire reports and XSLT reports from maven
+    
+{
+   @Test
+   
+   public void sendMail(){
+	   
+	   System.out.println("*****App Test********");
+	   
+   }
+}
---
+++ src/test/java/com/testnglearning/SampleTest.java
@@ -0,0 +1,13 @@
+package com.testnglearning;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+	@Test
+	public void sampletest(){
+		   
+		   System.out.println("*****Sample Test********");
+		   
+	   }
+}
